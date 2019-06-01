5d8e8c3175cb31327bf242e98509f03322d785b6
Clint Daniels <clint.daniels@wsp.com>
2019-05-31 17:09:20 -0700

    Interim commit.

--- simulation.py
+++ simulation.py
@@ -19,3 +19,32 @@ from activitysim.core import tracing
 from activitysim.core import config
 from activitysim.core.config import setting
 from activitysim.core import pipeline
+
+
+def run():
+    config.handle_standard_args()
+
+    # specify None for a pseudo random base seed
+    # inject.add_injectable('rng_base_seed', 0)
+
+    tracing.config_logger()
+    config.filter_warnings()
+
+    tracing.delete_csv_files()
+
+    # If you provide a resume_after argument to pipeline.run
+    # the pipeline manager will attempt to load checkpointed tables from the checkpoint store
+    # and resume pipeline processing on the next submodel step after the specified checkpoint
+    resume_after = setting('resume_after', None)
+
+    if resume_after:
+        print("resume_after", resume_after)
+
+    pipeline.run(models=setting('models'), resume_after=resume_after)
+
+    # tables will no longer be available after pipeline is closed
+    pipeline.close_pipeline()
+
+
+if __name__ == '__main__':
+    run()
