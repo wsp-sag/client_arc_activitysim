1d2109be7af4361b70beef0260cdad7da87aeb55
chryssac <chryssac@yahoo.com>
2019-06-24 08:55:24 -0600

    modified script. Code selects only the first 25 zones from the land data. Then 25 households that are in those zones and 25 persons that are in those households

--- scripts/build_h5.py
+++ scripts/build_h5.py
@@ -6,21 +6,20 @@ arc_h5_file = os.path.join('..', 'data', 'arc_asim.h5')
 # add the first 25 rows to the h5 file
 land_data = pd.read_csv(r'/Users/chryssac/git/client_arc_activitysim/data/ZoneData.csv')
 land_chunk = land_data[0:25]
-print(land_chunk)
+#print(land_chunk)
 land_chunk.to_hdf(arc_h5_file, 'land_use_taz', mode='w')
 
 # add the households that are in the same zones as the land data selected
 households = pd.read_csv(r'/Users/chryssac/git/client_arc_activitysim/data/households.csv')
 households_chunk = households[households['maz'].between(0, 24)]
-print(households_chunk)
-
-households_chunk.to_hdf(arc_h5_file, 'households', mode='a')
-
-hhids = households_chunk['HHID']
+households_subset = households_chunk.sample(n=25)
+#print(households_subset)
+households_subset.to_hdf(arc_h5_file, 'households', mode='a')
 
 # add the persons that are in those households
 persons = pd.read_csv(r'/Users/chryssac/git/client_arc_activitysim/data/persons.csv')
-
-persons_chunk = persons[persons['HHID'].between(hhids.min(), hhids.max())]
-print(persons_chunk)
-persons_chunk.to_hdf(arc_h5_file, 'persons', mode='a')
+hhids = households_subset['HHID']
+persons_chunk = persons.loc[persons['HHID'].isin(hhids)]
+persons_subset = persons_chunk.sample(n=25)
+#print(persons_subset)
+persons_subset.to_hdf(arc_h5_file, 'persons', mode='a')
